adc2861ad97a2c8ab195d81fe816849947aba2d8
Mohd Shaib <shaj.native@gmail.com>
2020-10-22 15:57:21 +0530

    this scope fixed

--- src/components/App.js
+++ src/components/App.js
@@ -2,17 +2,31 @@ import React, { Component, useState } from "react";
 import "../styles/App.css";
 
 class App extends Component {
-  handleClick = () => {
-    let element = document.getElementById("para");
-    element.innerText =
-      "Hello, I've learnt to use the full-stack evaluation tool. This makes me so happy";
-  };
+  constructor(props) {
+    super(props);
+    this.state = {
+      display: false
+    };
+    this.handleClick = this.handleClick.bind(this);
+  }
+  handleClick() {
+    this.setState({
+      display: true
+    });
+  }
 
   render() {
     return (
       <div id="main">
         <button id="click" onClick={this.handleClick}></button>
-        <p id="para"></p>
+        {this.state.display ? (
+          <p id="para">
+            Hello, I've learnt to use the full-stack evaluation tool. This makes
+            me so happy
+          </p>
+        ) : (
+          ""
+        )}
       </div>
     );
   }
